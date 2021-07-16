a6627294d032652d813bae7dbc7566c8c5c1c115
Erick Fernandes Harada - EHA <erick.harada@iteris.com.br>
2021-07-16 09:52:06 -0300

    Working with Context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Login from './components/login';
+import Profile from './components/profile';
+import { useAuth } from './providers/auth';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const { user } = useAuth();
+    console.log(user);
+
+    return (
+        <div className="App">
+            <h1>Ola, mundo</h1>
+            <Profile />
+            <Login />
+        </div>
+    );
 }
 
 export default App;
